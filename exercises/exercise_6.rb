70095c2669b6687ab2f61e7698e7995cda8c9c33
Keith Millar <keithmillar@hotmail.ca>
2020-07-23 05:39:58 +0000

    the rest of the exercises

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Wally", last_name: "World", hourly_rate: 50)
+@store1.employees.create(first_name: "Cheddar", last_name: "Biscuit", hourly_rate: 6)
+@store2.employees.create(first_name: "Keith", last_name: "Millar", hourly_rate: 44)
+@store2.employees.create(first_name: "Buddy", last_name: "Pal", hourly_rate: 130)
+@store2.employees.create(first_name: "Corey", last_name: "Henn", hourly_rate: 20)
